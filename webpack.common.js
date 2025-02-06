2c7b5639434a79eff11d6ebbbd7482a1e4effec5
Musab OD <Musab.odeh.7@gmail.com>
2025-02-06 18:39:45 +0300

    remove babel from template

--- webpack.common.js
+++ webpack.common.js
@@ -15,13 +15,6 @@ plugins: [
 module: {
     rules: [
     {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-            loader: "babel-loader"
-        }
-    },
-    {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
     },
